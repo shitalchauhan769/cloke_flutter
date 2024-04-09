6c9cb4d34f9711b7e2b34083d5c3b1255bb33605
R82 <shitalchauhan769@gmail.com>
2024-04-09 08:13:18 +0530

    9-4-2024

--- lib/screen/cloke_screen.dart
+++ lib/screen/cloke_screen.dart
@@ -12,15 +12,18 @@ class ClockScreen extends StatefulWidget {
 class _ClockScreenState extends State<ClockScreen> {
   DateTime time = DateTime.now();
   var scaffoldkey = GlobalKey<ScaffoldState>();
+  bool Isdigital = false;
+  bool Isanulog = false;
+  bool Isstrap = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       key: scaffoldkey,
       child: Scaffold(
-        backgroundColor: Color(0xff1a4066),
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text("ClockScreen", style: TextStyle(fontSize: 20)),
+          title: const Text("ClockScreen",),
           leading: Builder(
             builder: (context) {
               return IconButton(
@@ -33,62 +36,111 @@ class _ClockScreenState extends State<ClockScreen> {
         ),
         body: Center(
           child: Stack(
+            alignment: Alignment.center,
             children: [
-              Stack(
-                children: List.generate(
-                  60,
-                  (index) => Transform.rotate(
-                    angle: (pi * 2 * index) / 60,
-                    child: Divider(
-                      thickness: 2,
-                      endIndent: index%5==0?MediaQuery.sizeOf(context).width * 0.90:MediaQuery.sizeOf(context).width * 0.95,
-                      color: index%5==0?Colors.red:Colors.black,
+              //Strop clock
+              Visibility(
+                visible: Isstrap,
+                child: Stack(alignment: Alignment.center, children: [
+                  //second
+                  Container(
+                    height: 260,
+                    width: 260,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 15,
+                      value: time.second * 1 / 60,
+                      color: Colors.pink,
+                      backgroundColor: Colors.pink.withOpacity(0.2),
                     ),
                   ),
-                ),
-              ),
-              //hour
-              Transform.rotate(
-                angle: pi/2,
-                child: Transform.rotate(
-                  angle: ((pi*2)*time.hour)/12,
-                  child: Divider(
-                    thickness: 6,
-                    indent: 35,
-                    endIndent: MediaQuery.sizeOf(context).width*0.50,
-                    color: Colors.orange,
+                  //minuet
+                  Container(
+                    height: 210,
+                    width: 210,
+                    child: CircularProgressIndicator(
+                      value: time.minute * 1 / 60,
+                      color: Colors.green,
+                      backgroundColor: Colors.green.withOpacity(0.2),
+                      strokeWidth: 15,
+                    ),
                   ),
-                ),
-              ),
-              //minute
-              Transform.rotate(
-                angle: pi/2,
-                child: Transform.rotate(
-                  angle: ((pi*2)*time.minute)/60,
-                  child: Divider(
-                    thickness: 3,
-                    indent: 19,
-                    endIndent: MediaQuery.sizeOf(context).width*0.50,
-                    color: Colors.green,
+                  //hour
+                  CircularProgressIndicator(
+                    value: time.hour * 1 / 12,
+                    color: Colors.yellow,
+                    backgroundColor: Colors.yellow.withOpacity(0.2),
+                    strokeWidth: 15,
                   ),
-                ),
+
+
+                ]),
               ),
-              //seconds
-              Transform.rotate(
-                angle: pi/2,
-                child: Transform.rotate(
-                  angle: ((pi*2)*time.second)/60,
-                  child: Divider(
-                    thickness: 3,
-                    endIndent: MediaQuery.sizeOf(context).width*0.50,
-                    color: Colors.red,
-                  ),
+
+              //analog clock
+              Visibility(
+                visible: Isanulog,
+                child: Stack(
+                  children: [
+                    Stack(
+                      children: List.generate(
+                        60,
+                            (index) => Transform.rotate(
+                          angle: (pi * 2 * index) / 60,
+                          child: Divider(
+                            thickness: 2,
+                            endIndent: index % 5 == 0
+                                ? MediaQuery.sizeOf(context).width * 0.90
+                                : MediaQuery.sizeOf(context).width * 0.95,
+                            color: index % 5 == 0 ? Colors.red : Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: pi / 2,
+                      child: Transform.rotate(
+                        angle: ((pi * 2) * time.hour) / 12,
+                        child: Divider(
+                          thickness: 6,
+                          indent: 35,
+                          endIndent: MediaQuery.sizeOf(context).width * 0.50,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                    //minute
+                    Transform.rotate(
+                      angle: pi / 2,
+                      child: Transform.rotate(
+                        angle: ((pi * 2) * time.minute) / 60,
+                        child: Divider(
+                          thickness: 3,
+                          indent: 19,
+                          endIndent: MediaQuery.sizeOf(context).width * 0.50,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    //seconds
+                    Transform.rotate(
+                      angle: pi / 2,
+                      child: Transform.rotate(
+                        angle: ((pi * 2) * time.second) / 60,
+                        child: Divider(
+                          thickness: 3,
+                          endIndent: MediaQuery.sizeOf(context).width * 0.50,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+
+                  ],
                 ),
               ),
 
               //digital Clock
               Visibility(
-                visible: false,
+                visible: Isdigital,
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
@@ -131,48 +183,122 @@ class _ClockScreenState extends State<ClockScreen> {
             ],
           ),
         ),
-        drawer: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const Text("DrvearScreen",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold)),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 120,
-                width: 120,
-                decoration: const BoxDecoration(
-                    color: Colors.black, shape: BoxShape.circle),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Column(
-                children: [
-                  Text(
-                    "profile name",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 40,),
-                  Icon(Icons.watch_later_outlined,color: Colors.white),
-                  Text(
-                    "Digital Clocke",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ],
+        drawer: Drawer(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.grey.shade300),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("profilename",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                 Row(
+                  children: [
+                    const Text(
+                      "Digital clock",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(
+                                () {
+                              Isstrap = false;
+                              Isanulog = false;
+                              Isdigital = true;
+                            },
+                          );
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.watch_later_outlined,
+                          color: Colors.black,
+                        ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                 Row(
+                  children: [
+                    const Text(
+                      "Analog clock",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(
+                                () {
+                              Isstrap = false;
+                              Isanulog = true;
+                              Isdigital = false;
+                            },
+                          );
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.watch_later_outlined,
+                          color: Colors.black,
+                        ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Strap clock",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(
+                            () {
+                              Isstrap = true;
+                              Isanulog = false;
+                              Isdigital = false;
+                            },
+                          );
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.watch_later_outlined,
+                          color: Colors.black,
+                        ))
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
